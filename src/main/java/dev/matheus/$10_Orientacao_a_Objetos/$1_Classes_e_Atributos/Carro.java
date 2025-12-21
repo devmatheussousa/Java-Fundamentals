cdb40f9352da3847004997fe9cd83d65e3fc4b72
DevMatheusSousa <dev.matheus.contato@gmail.com>
2025-12-21 16:01:34 -0300

    feat: Classes e Objetos

---
+++ src/main/java/dev/matheus/$10_Orientacao_a_Objetos/$1_Classes_e_Atributos/Carro.java
@@ -0,0 +1,19 @@
+package dev.matheus.$10_Orientacao_a_Objetos.$1_Classes_e_Atributos;
+
+/**
+ * Classe que representa um carro.
+ * Obejto: e uma instancia da classe com Operador new
+ * Atributos: Caracteristicas do objeto
+ * Modelo: Marca e tipo do carro
+ * Cor: Cor do carro
+ * Ano: Ano de fabricação do carro etc.
+ */
+public class Carro {
+    //Oque e clean code -> E forma de escrever e organizar nosso codigos de forma que seja facil de entender e de manter
+    // Nome de variaveis -> Deve ser descritivo e intuitivo
+    String marca;
+    String modelo;
+    int numPassageiros; // Quantidade de passageiros que o carro pode transportar
+    double capCombustivel; // Capacidade de combustivel do carro em litros
+    double consumoCombustivel; // Consumo de combustivel do carro em km/litro
+}
---
+++ src/main/java/dev/matheus/$10_Orientacao_a_Objetos/$1_Classes_e_Atributos/TesteCarro.java
@@ -0,0 +1,41 @@
+package dev.matheus.$10_Orientacao_a_Objetos.$1_Classes_e_Atributos;
+
+public class TesteCarro {
+    public static void main(String[] args) {
+        // Criando um objeto da classe Carro
+        Carro van = new Carro();
+        van.marca = "Ford";
+        van.modelo = "Ford";
+        van.numPassageiros = 8;
+        van.capCombustivel = 90.0;
+        van.consumoCombustivel = 14.0;
+
+        Carro fusca = new Carro();
+        fusca.marca = "Volkswagen";
+        fusca.modelo = "Fusca";
+        fusca.numPassageiros = 5;
+        fusca.capCombustivel = 40.0;
+        fusca.consumoCombustivel = 11.0;
+
+
+
+        // Imprimindo as informações do carro
+        System.out.printf("""
+                        Marca van: %s
+                        Modelo van: %s
+                        Numero de Passageiros van: %d
+                        Capacidade de Combustivel van: %.2f
+                        Consumo de Combustivel van: %.2f
+                        <p/>
+                        Marca fusca: %s
+                        Modelo fusca: %s
+                        Numero de Passageiros fusca: %d
+                        Capacidade de Combustivel fusca: %.2f
+                        Consumo de Combustivel fusca: %.2f
+                        <p/>
+                        """, van.marca, van.modelo, van.numPassageiros, van.capCombustivel, van.consumoCombustivel,
+                                fusca.marca, fusca.modelo, fusca.numPassageiros, fusca.capCombustivel, fusca.consumoCombustivel
+
+        );
+    }
+}
