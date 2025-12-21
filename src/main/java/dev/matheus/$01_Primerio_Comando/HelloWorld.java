b8d61df93c8d31c6e44a1b3011565b4e6f8a4db1
DevMatheusSousa <dev.matheus.contato@gmail.com>
2025-12-21 08:02:03 -0300

    feat: Primeiro programa Java

---
+++ src/main/java/dev/matheus/$01_Primerio_Comando/HelloWorld.java
@@ -0,0 +1,9 @@
+package dev.matheus.$01_Primerio_Comando;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Primeiro aplicativo em Java!");
+    }
+}
